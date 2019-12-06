8d5357488f5a37d087933ee33cfb2a9b90521bc0
james lawson <jlawson079@gmail.com>
2019-12-05 22:46:38 -0800

    Compling, bundling and a closer look at JSX

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('div',null,'React Element!');//creates react Element Object, .creatElement is included in React CDN
+// const element = React.createElement('div',null,'React Element!');//creates react Element Object, .creatElement is included in React CDN
+const element = <div>React Element with JSX </div>
 
 ReactDOM.render(element, document.getElementById('root')); //document.body is bad practice (per the error message wisdome lol) better to access from .html file instead directly through parent elements. 
